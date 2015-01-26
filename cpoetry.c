7d089f63e3e36a6e75d618b3ad960780ae024ad2
Marrub <marrub@greyserv.net>
2015-01-26 08:57:19 +0000

    ghgfh

---
+++ cpoetry.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "cpoetry.h"
+
+The Raven
+  Much I marvelled this ungaily fowl to hear discourse so plainly,
+  Though its answer little meaning -- little relevancy bore;
+  For we cannot help agreeing that no sublunary being
+  Ever yet was blessed with seeing bird above his chamber door --
+  Bird or beast upon the sculptured bust above his chamber door,
+  With such name as Nevermore
---
+++ cpoetry.h
@@ -0,0 +1,53 @@
+#define Much int i=0;puts("Much "
+#define I "I "
+#define marvelled "marvelled this ungaily fowl "
+#define this "this "
+#define ungaily
+#define fowl
+#define to "to "
+#define hear "hear discourse so "
+#define discourse
+#define so
+#define plainly "plainly,")
+#define Though fputs
+#define its ("Though its answer little meaning --", stdout);
+#define answer
+#define little
+#define meaning func(i
+#define relevancy ); puts(" little relevancy"
+#define bore " bore;")
+#define The void func(int i) {\
+if(i== -1)puts(" --");if(i== -2)puts(",");} int
+#define Raven main() {
+#define For fputs("For"
+#define we " we "
+#define cannot "cannot "
+#define help "help "
+#define agreeing "agreeing "
+#define that "that "
+#define no "no "
+#define sublunary "sublunary "
+#define being "being\n"
+#define Ever "Ever "
+#define yet "yet "
+#define was "was "
+#define blessed "blessed "
+#define with "with "
+#define seeing "seeing "
+#define bird "bird "
+#define above "above "
+#define his "his "
+#define chamber "chamber "
+#define door "door",stdout); func((i
+#define Bird )); fputs("Bird "
+#define or "or "
+#define beast "beast "
+#define upon "upon "
+#define the "the "
+#define sculptured "sculptured "
+#define bust "bust "
+#define With i++)); fputs("With "
+#define such "such "
+#define name "name as "
+#define as "as "
+#define Nevermore "Nevermore\n", stdout);}
